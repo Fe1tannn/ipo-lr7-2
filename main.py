7c2294e39351e5acd1d516f0ca968f819feef520
Yan <tal531764@gmail.com>
2024-12-10 02:21:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+num = str(input("Введите номер квалификации: "))
+file = 'dump.json'
+skills = False
+with open(file, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    for skill in data:
+        if skill.get("model") == "data.skill":
+            if skill["fields"].get("code") == num:
+                skill_code = skill["fields"].get("code")
+                skill_title = skill["fields"].get("title")
+                skills = True
+                for profession in data:
+                    if profession.get("model") == "data.specialty":
+                        specialty_code = profession["fields"].get("code")
+                        if specialty_code in num:  
+                            specialty_title = profession["fields"].get("title")
+                            specialty_educational = profession["fields"].get("c_type")
+                            break
+                break
+if not skills:
+    print("=============== Не Найдено ===============") 
+else:
+    print("=============== Найдено ===============") 
+    print(f"{specialty_code} >> Специальность {specialty_title} , {specialty_educational}")
+    print(f"{skill_code} >> Квалификация {skill_title}")
