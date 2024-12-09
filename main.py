7054e4b48a362e2f42034d22160c3facc33eeacb
Yan <tal531764@gmail.com>
2024-12-10 02:26:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import json
-num = str(input("Введите номер квалификации: "))
-file = 'dump.json'
-skills = False
-with open(file, 'r', encoding='utf-8') as file:
-    data = json.load(file)
-    for skill in data:
+import json #Библиотека
+num = str(input("Введите номер квалификации: ")) #Ввод искомой класификации
+file = 'dump.json' #Переменная с названием файла
+skills = False #Переменная для проверки
+with open(file, 'r', encoding='utf-8') as file: #Открываем файл 
+    data = json.load(file) #Сохраняем файл в переменную 
+    for skill in data: #Перебор
         if skill.get("model") == "data.skill":
-            if skill["fields"].get("code") == num:
+            if skill["fields"].get("code") == num: #Условие
                 skill_code = skill["fields"].get("code")
                 skill_title = skill["fields"].get("title")
                 skills = True
-                for profession in data:
-                    if profession.get("model") == "data.specialty":
+                for profession in data: #Перебор
+                    if profession.get("model") == "data.specialty": #Условие
                         specialty_code = profession["fields"].get("code")
-                        if specialty_code in num:  
+                        if specialty_code in num:  #Условие
                             specialty_title = profession["fields"].get("title")
                             specialty_educational = profession["fields"].get("c_type")
-                            break
+                            break #Выход
                 break
-if not skills:
+if not skills: 
     print("=============== Не Найдено ===============") 
 else:
     print("=============== Найдено ===============") 
